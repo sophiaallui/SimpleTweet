9f9adb8409c31297fab45fa85b8f8ed826724ec7
sophiaallui <solui@ucsd.edu>
2020-11-05 18:33:53 -0800

    Simple Tweet Part 2: Composing and Publishing Tweets with live view on timeline

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -0,0 +1,97 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.parceler.Parcels;
+
+import okhttp3.Headers;
+
+public class ComposeActivity extends AppCompatActivity {
+
+    public static final int MAX_TWEET_LENGTH = 280;
+    public static final String TAG = "ComposeActivity";
+
+    EditText etCompose;
+    Button btnTweet;
+    TwitterClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+
+        etCompose = findViewById(R.id.etCompose);
+        btnTweet = findViewById(R.id.btnTweet);
+        client = TwitterApp.getRestClient(this);
+
+
+        // Click listener, when tapped, to publish it
+
+        // Set click listener on button
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // if too long...
+                String tweetContent = etCompose.getText().toString();
+
+                if (tweetContent.isEmpty()) {
+                    Toast.makeText(ComposeActivity.this, "Empty Tweet", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (tweetContent.length() > MAX_TWEET_LENGTH) {
+                    Toast.makeText(ComposeActivity.this, "Tweet exceeds 280 characters", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
+
+                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.i(TAG, "OnSuccess to publish tweet");
+                        //json object
+                        try {
+                            Tweet tweet = Tweet.fromJson(json.jsonObject);
+                            Log.i(TAG, "Published Tweet says " + tweet.body);
+                            Intent intent = new Intent();
+                            intent.putExtra("tweet", Parcels.wrap(tweet));
+                            setResult(RESULT_OK, intent);
+                            finish();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.e(TAG, "onFailure to publish tweet", throwable);
+                    }
+                });
+            }
+        });
+        // Make an API call to Twitter to publish the tweet
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
@@ -45,7 +45,7 @@ public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 	// i.e Display application "homepage"
 	@Override
 	public void onLoginSuccess() {
-		Log.i("yayaaya","loginn success");
+		//Log.i("yayaaya","loginn success");
 		 Intent i = new Intent(this, TimelineActivity.class);
 		 startActivity(i);
 	}
